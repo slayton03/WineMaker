df39c12930916b4a39cd01d378325642a58b55d5
Stephen <stephenlayton3@gmail.com>
2023-04-03 09:44:18 -0400

    Attempted to add home labels

--- WorkEnvironment/MainUI.py
+++ WorkEnvironment/MainUI.py
@@ -47,25 +47,35 @@ class App(customtkinter.CTk):
         self.note_btn = customtkinter.CTkButton(self.sidebar_frame, command=self.sidebar_button_event)
         self.note_btn.grid(row=5, column=0, padx=20, pady=10)
         self.note_btn.configure(text="Notes")
-
+        # Create apperance label
         self.appearance_mode_label = customtkinter.CTkLabel(self.sidebar_frame, text="Appearance Mode:", anchor="w")
         self.appearance_mode_label.grid(row=7, column=0, padx=20, pady=(10, 0))
+        # Create apperance dropdown
         self.appearance_mode_optionemenu = customtkinter.CTkOptionMenu(self.sidebar_frame, values=["Light", "Dark", "System"],
                                                                        command=self.change_appearance_mode_event)
         self.appearance_mode_optionemenu.grid(row=8, column=0, padx=20, pady=(10, 10))
-        self.scaling_label = customtkinter.CTkLabel(self.sidebar_frame, text="UI Scaling:", anchor="w")
-        self.scaling_label.grid(row=9, column=0, padx=20, pady=(10, 0))
-        self.scaling_optionemenu = customtkinter.CTkOptionMenu(self.sidebar_frame, values=["80%", "90%", "100%", "110%", "120%"],
-                                                               command=self.change_scaling_event)
-        self.scaling_optionemenu.grid(row=10, column=0, padx=20, pady=(10, 20))
+
+
+
+        # Home label
+        self.home_label = customtkinter.CTkLabel(self, text="Home",
+                                                 font=customtkinter.CTkFont(size=30, weight="bold"))
+        self.home_label.grid(row=0, column=1, padx=(20,0), pady=(20, 10))
+        self.sidebar_frame.grid_rowconfigure(2, weight=1)
+
+        # to-do label
+        self.todo_label = customtkinter.CTkLabel(self, text="To-Do",
+                                                 font=customtkinter.CTkFont(size=30, weight="bold"))
+        self.todo_label.grid(row=0, column=3, padx=(20, 0), pady=(20, 10))
+
+        # to-do label
+        self.todo2_label = customtkinter.CTkLabel(self, text="To-Do",
+                                                 font=customtkinter.CTkFont(size=30, weight="bold"))
+        self.todo2_label.grid(row=0, column=4, padx=(20, 0), pady=(20, 10))
+
 
     def change_appearance_mode_event(self, new_appearance_mode: str):
         customtkinter.set_appearance_mode(new_appearance_mode)
-
-    def change_scaling_event(self, new_scaling: str):
-        new_scaling_float = int(new_scaling.replace("%", "")) / 100
-        customtkinter.set_widget_scaling(new_scaling_float)
-
     def sidebar_button_event(self):
         print("sidebar_button click")
 
